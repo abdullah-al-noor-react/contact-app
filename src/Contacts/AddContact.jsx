13d2231274a2bc4912f8c3cb8ab03c85fcc4df60
abdullahalnoor <aalnoor74@gmail.com>
2022-03-24 02:21:15 +0600

    first commit

--- src/Contacts/AddContact.jsx
+++ src/Contacts/AddContact.jsx
@@ -1,25 +1,69 @@
-import React from 'react'
+import {useState} from 'react'
 
 import {Form,Button} from 'react-bootstrap'
 
 function AddContact() {
+    const contactObject = {
+        first_name:"",
+        last_name:"",
+        email:"",
+        phone:"",
+        dob:'',
+        picture:"",
+        gender:"male"
+    }
+    const [contact,setContact] = useState(contactObject)
+
+const handleChange = (e)=>{
+    setContact(prevState => {
+        return{
+            ...prevState,
+            [e.target.name]:e.target.value
+        }
+    })
+}
+  
+
+const  {first_name,last_name,email,phone,dob,picture,gender} = contact
+
   return (
     <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>First Name </Form.Label>
+                <Form.Control type="text" placeholder="Enter First Name"
+                name="first_name" value={first_name} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Last Name </Form.Label>
+                <Form.Control type="text" placeholder="Enter Last Name"
+                name="last_name" value={last_name} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Email </Form.Label>
+                <Form.Control type="email" placeholder="Enter Email"
+                name="email" value={email} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Phone </Form.Label>
+                <Form.Control type="text" placeholder="Enter Phone"
+                name="phone" value={phone} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>DOB </Form.Label>
+                <Form.Control type="date" placeholder="Enter DOB"
+                name="dob" value={dob} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group className="mb-3" >
+                <Form.Label>Picture </Form.Label>
+                <Form.Control type="tex" placeholder="Enter Picture"
+                name="picture" value={picture} onChange={handleChange} />
+            </Form.Group>
+                <Form.Select className="mb-3" onChange={handleChange}  name="gender">
+                    <option>  Select Gender</option>
+                    <option value="male"  >Male</option>
+                    <option value="female" >Female</option>
+                </Form.Select>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
         <Button variant="primary" type="submit">
             Submit
         </Button>
